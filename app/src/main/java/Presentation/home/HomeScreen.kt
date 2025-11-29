157cd3b05367a8f41050f5081fc220243f49f313
sohaila <sohailahendawy8@gmail.com>
2025-11-29 20:59:43 +0200

    Sohaila

--- app/src/main/java/Presentation/home/HomeScreen.kt
+++ app/src/main/java/Presentation/home/HomeScreen.kt
@@ -1,52 +1,108 @@
 package Presentation.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
+import com.example.habittracker.presentation.onboarding.OnboardingScreen
+import com.example.screenone.ui.theme.LightBeige
+import com.example.screenone.ui.theme.MoeGreen
+import com.example.screenone.ui.theme.TextWhite
 
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel,
     onNewHabitClick: () -> Unit
 ) {
-    val state = viewModel.state.collectAsState().value
-
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 32.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .background(LightBeige)
+            .padding(24.dp)
     ) {
-
-        Text(
-            text = "Welcome back!",
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold
-        )
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Button(
-            onClick = onNewHabitClick,
+        val state = viewModel.state.collectAsState().value
+        Column(
             modifier = Modifier
-                .height(45.dp)
-                .width(180.dp)
+                .fillMaxSize() // 1. Fills the entire screen height
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally, // 2. Centers items horizontally
+            verticalArrangement = Arrangement.Center // 3. Centers items vertically (Middle of screen)
         ) {
-            Text(text = "New Habit   +")
+
+            Row(
+
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Image(
+                    painter = painterResource(id = com.example.screenone.R.drawable.cup),
+                    contentDescription = "cup_image",
+                    modifier = Modifier
+                        .size(150.dp)
+                )
+
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+
+                Image(
+                    painter = painterResource(id = com.example.screenone.R.drawable.polygon1),
+                    contentDescription = "polygon_image",
+                    modifier = Modifier
+                        .size(150.dp)
+                )
+            }
         }
 
-        Spacer(modifier = Modifier.height(120.dp))
 
-        if (state.habits.isEmpty()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
             Text(
-                text = "No habits yet. Add your first habit!",
-                fontSize = 18.sp
+                text = "Welcome back!",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
             )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+
+            Button(
+                onClick = onNewHabitClick,
+                colors = ButtonDefaults.buttonColors(containerColor = MoeGreen),
+                shape = RoundedCornerShape(50),
+                modifier = Modifier
+                    .height(45.dp)
+                    .width(180.dp)
+            ) {
+                Text(text = "New Habit   +")
+            }
+
+            Spacer(modifier = Modifier.height(120.dp))
+
+            if (state.habits.isEmpty()) {
+                Text(
+                    text = "No habits yet. Add your first habit!",
+                    fontSize = 18.sp
+                )
+            }
         }
     }
 }
